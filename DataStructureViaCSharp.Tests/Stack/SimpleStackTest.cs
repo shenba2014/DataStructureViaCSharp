136545c409dfa9a45b837e58b11d7e6ac37f049d
leo.luo <luojunwenbashen@163.com>
2018-11-04 22:48:24 +0800

    simple stack praparation

---
+++ DataStructureViaCSharp.Tests/Stack/SimpleStackTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructureViaCSharp.Stack;
+using Xunit;
+
+namespace DataStructureViaCSharp.Tests.Stack
+{
+	public class SimpleStackTest
+	{
+		[Fact]
+		public void ShouldOutputElementInReverseOrder()
+		{
+			var stack = new SimpleStack<int>(10);
+			stack.Push(1);
+			stack.Push(2);
+
+			var value = stack.Pop();
+			Assert.Equal(2, value);
+
+			value = stack.Pop();
+			Assert.Equal(1, value);
+		}
+
+		[Fact]
+		public void ShouldPushFailedWhenStackIsFull()
+		{
+			var stack = new SimpleStack<int>(2);
+			stack.Push(1);
+			stack.Push(2);
+			var result = stack.Push(3);
+
+			Assert.False(result);
+		}
+
+		[Fact]
+		public void ShouldPopDefaultValueWhenStackIsEmpty()
+		{
+			var stack = new SimpleStack<object>(2);
+			var result = stack.Pop();
+
+			Assert.Null(result);
+		}
+	}
+}
---
+++ DataStructureViaCSharp/Stack/SimpleStack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureViaCSharp.Stack
+{
+	public class SimpleStack<TData>
+	{
+		private readonly TData[] _values;
+
+		private int _count;
+
+		public SimpleStack(int capacity)
+		{
+			_values = new TData[capacity];
+		}
+
+		public bool Push(TData element)
+		{
+			if (_count + 1 > _values.Length)
+				return false;
+
+			_values[_count++] = element;
+			return true;
+		}
+
+		public TData Pop()
+		{
+			if (_count == 0)
+				return default(TData);
+
+			var value = _values[_count - 1];
+			_count--;
+
+			return value;
+		}
+	}
+}
